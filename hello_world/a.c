b2e5fc1f69e3e0a065abfc9e70b0ec1076a73092
megahomyak <g.megahomyak@gmail.com>
2024-06-26 05:31:58 +0500

    "Hello, world!" successfully made!

--- hello_world/a.c
+++ hello_world/a.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include "metadata.h"
 
 int main() {
-    printf("Hello, world!");
+    printf("Hello, world!\n");
+    printf(
+        "My version is %li.%li.%li\n",
+        version.major, version.minor, version.patch
+    );
     return 0;
 }
---
+++ hello_world/metadata.template.h
@@ -0,0 +1,5 @@
+const struct {
+    long major;
+    long minor;
+    long patch;
+} version = {@hello_world_VERSION_MAJOR@, @hello_world_VERSION_MINOR@, @hello_world_VERSION_PATCH@};
